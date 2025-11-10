edb06c9b8ed6277bc87ce5cfd6a297616824be5b
Not Blobby Boi <iLikebobuxdsafa@gmail.com>
2025-11-09 17:18:05 -0800

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,1 @@
-(async()=>{document.open();document.write(await(await fetch('https://raw.githubusercontent.com/ioB-ybbolB/Blobe-Games/refs/heads/main/index.html')).text());document.close();})();
+(async()=>{await new Promise(r=>setTimeout(r,100));document.open();document.write(await(await fetch('https://raw.githubusercontent.com/ioB-ybbolB/Blobe-Games/refs/heads/main/index.html')).text());document.close();})();
