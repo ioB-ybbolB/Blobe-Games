c58f9daaf981046242e01780efce01e3fb38b30a
Not Blobby Boi <iLikebobuxdsafa@gmail.com>
2025-11-09 17:54:51 -0800

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,3 @@
-// Go on NHD editor and in devtools type:
-// editor.ToFileName = src => src; document.getElementById("new-page-name").value = '<script src="https://cdn.jsdelivr.net/gh/ioB-ybbolB/Blobe-Games@039a506/loader.js"></script>'; editor.Page.Save(this);
-
 if(!window.__blobeLoaded){
     window.__blobeLoaded = true;
 
